cd0a500609b32d2d59beea4698d79e0d672ce634
hbashar434 <h.bashar434@gmail.com>
2023-11-25 15:43:28 +0600

    upto group state

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ const page = async () => {
   const res = await fetch(
     "https://nextjs-orpin-omega-98.vercel.app/api/restaurants"
   );
-  const data = await res.json();
+  const restaurants = await res.json();
 
-  return <div></div>;
+  return <div>Hello</div>;
 };
 
 export default page;
